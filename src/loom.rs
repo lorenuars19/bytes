addf12e095a77404ec91826456c873392f412f90
lorenuar <lorenuar.s19@gmail.com>
2023-07-03 14:16:51 +0000

    try atomic-polyfills instead

--- src/loom.rs
+++ src/loom.rs
@@ -1,17 +1,23 @@
 #[cfg(not(all(test, loom)))]
 pub(crate) mod sync {
     pub(crate) mod atomic {
-        #[cfg(not(feature = "extra-platforms"))]
-        pub(crate) use core::sync::atomic::*;
-        #[cfg(feature = "extra-platforms")]
-        pub(crate) use portable_atomic::*;
+        #[cfg(not(feature = "use-atomic-polyfill"))]
+        pub(crate) use core::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 
+        #[cfg(feature = "use-atomic-polyfill")]
+        pub(crate) use atomic_polyfill::{AtomicPtr, AtomicUsize, Ordering};
+        
         pub(crate) trait AtomicMut<T> {
-            fn with_mut<F, R>(&mut self, f: F) -> R where F: FnOnce(&mut *mut T) -> R;
+            fn with_mut<F, R>(&mut self, f: F) -> R
+            where
+                F: FnOnce(&mut *mut T) -> R;
         }
 
         impl<T> AtomicMut<T> for AtomicPtr<T> {
-            fn with_mut<F, R>(&mut self, f: F) -> R where F: FnOnce(&mut *mut T) -> R {
+            fn with_mut<F, R>(&mut self, f: F) -> R
+            where
+                F: FnOnce(&mut *mut T) -> R,
+            {
                 f(self.get_mut())
             }
         }
@@ -21,7 +27,7 @@ pub(crate) mod sync {
 #[cfg(all(test, loom))]
 pub(crate) mod sync {
     pub(crate) mod atomic {
-        pub(crate) use loom::sync::atomic::{ AtomicPtr, AtomicUsize, Ordering };
+        pub(crate) use loom::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 
         pub(crate) trait AtomicMut<T> {}
     }
